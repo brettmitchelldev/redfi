5beebaea843c4b8e45fee9ea63ab435629d56eb3
Brett Mitchell <brett.mitchell@itential.com>
2024-05-29 14:48:51 -0400

    doc: update docstrings for flags

--- cmd/redfi.go
+++ cmd/redfi.go
@@ -9,9 +9,9 @@ import (
 )
 
 var (
-	planPath = flag.String("plan", "", "path to the plan.json")
-	server   = flag.String("redis", "127.0.0.1:6379", "address to the redis server, to proxy requests to")
-	addr     = flag.String("addr", "127.0.0.1:8083", "address for the proxy to listen on")
+	planPath = flag.String("plan", "", "Path to the plan file, must be formatted as JSON")
+	server   = flag.String("redis", "127.0.0.1:6379", "Address of the target Redis server, to proxy requests to")
+	addr     = flag.String("addr", "127.0.0.1:8083", "Address for the proxy to listen on")
 )
 
 func main() {
