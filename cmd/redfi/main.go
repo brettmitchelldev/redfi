5ddba22b421dffea6994d4bc05e64073eb6717c1
Brett Mitchell <brett.mitchell@itential.com>
2024-06-10 13:51:04 -0400

    feat!: rename redis server address cli parameter

--- cmd/redfi/main.go
+++ cmd/redfi/main.go
@@ -9,17 +9,17 @@ import (
 )
 
 var (
-	planPath = flag.String("plan", "", "Path to the plan file, must be formatted as JSON")
-	server   = flag.String("redis", "127.0.0.1:6379", "Address of the target Redis server, to proxy requests to")
-	addr     = flag.String("addr", "127.0.0.1:6380", "Address for the proxy to listen on")
-	apiAddr  = flag.String("api", "127.0.0.1:8081", "Address for the HTTP API to listen on")
-	logging  = flag.String("log", "", "Log level (give 'v' for verbose logging, 'vv' for very verbose)")
+	planPath  = flag.String("plan", "", "Path to the plan file, must be formatted as JSON")
+	redisAddr = flag.String("redis", "127.0.0.1:6379", "Address of the target Redis server, to proxy requests to")
+	addr      = flag.String("addr", "127.0.0.1:6380", "Address for the proxy to listen on")
+	apiAddr   = flag.String("api", "127.0.0.1:8081", "Address for the HTTP API to listen on")
+	logging   = flag.String("log", "", "Log level (give 'v' for verbose logging, 'vv' for very verbose)")
 )
 
 func main() {
 	flag.Parse()
 
-	proxy, err := redfi.New(*planPath, *server, *addr, *apiAddr, *logging)
+	proxy, err := redfi.New(*planPath, *redisAddr, *addr, *apiAddr, *logging)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
